9c034dad16bf2d65b0554a41f1b90c3f24e5061b
StephenPatrick <sythe2o0@gmail.com>
2017-10-18 17:48:12 -0500

    Better slides

--- core.go
+++ core.go
@@ -106,7 +106,7 @@ func main() {
 	oak.AddCommand("allShots", func([]string) {
 		for i := 0; i <= len(slides); i++ {
 			rgba := oak.ScreenShot()
-			f, err := os.Create("shot" + strconv.Itoa(shotIndex) + ".png")
+			f, err := os.Create("slides/slide" + strconv.Itoa(shotIndex) + ".png")
 			if err != nil {
 				dlog.Error(err)
 				return
@@ -114,8 +114,9 @@ func main() {
 			png.Encode(f, rgba)
 			dlog.ErrorCheck(f.Close())
 			shotIndex++
-			time.Sleep(100 * time.Millisecond)
+			time.Sleep(300 * time.Millisecond)
 			event.Trigger("KeyUpRightArrow", nil)
+			time.Sleep(300 * time.Millisecond)
 		}
 	})
 	show.AddNumberShortcuts(len(slides))
